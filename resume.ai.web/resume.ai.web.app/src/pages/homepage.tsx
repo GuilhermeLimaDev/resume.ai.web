3fe39620f13ad7a9ed2a1e48ee744f180905f104
Guilherme Lima <guilhermeguiguigugu2509@gmail.com>
2026-04-16 00:31:51 +0000

    first commit

---
+++ resume.ai.web/resume.ai.web.app/src/pages/homepage.tsx
@@ -0,0 +1,9 @@
+const Homepage = () =>{
+    return(
+        <main>
+            <p className="text-3xl font-bold underline text-blue-700">Ola, mundo</p>
+        </main>
+    )
+}
+
+export default Homepage
